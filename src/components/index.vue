d0152e0e037f9a3738a82ebe657525691421a5c4
yxt <390627223@qq.com>
2020-04-10 16:36:51 +0800

    index修改

--- src/components/index.vue
+++ src/components/index.vue
@@ -4,16 +4,16 @@
       <topHeader></topHeader>
       <el-main>
         <el-row :gutter="10">
-          <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
+          <el-col :xs="8" :sm="6" :md="4" :lg="4" :xl="1">
             <div class="grid-content bg-purple"></div>
           </el-col>
-          <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+          <el-col :xs="4" :sm="6" :md="8" :lg="10" :xl="11">
             <div class="grid-content bg-purple-light"></div>
           </el-col>
-          <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+          <el-col :xs="4" :sm="6" :md="8" :lg="10" :xl="11">
             <div class="grid-content bg-purple"></div>
           </el-col>
-          <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
+          <el-col :xs="8" :sm="6" :md="4" :lg="4" :xl="1">
             <div class="grid-content bg-purple-light"></div>
           </el-col>
         </el-row>
